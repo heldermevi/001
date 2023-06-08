e2790b4fe24ebc8b5bca372bfdf9421cbaeb4f60
Hélder Medeiros <helderapps@gmail.com>
2023-06-08 11:55:11 -0300

    aprendendo git

--- src/page/home/index.js
+++ src/page/home/index.js
@@ -4,7 +4,7 @@ import {StyleSheet, Text, View} from 'react-native';
 const LotsOfStyles = () => {
   return (
     <View style={styles.container}>
-      <Text style={styles.red}>just red</Text>
+      <Text style={styles.red}>ainda testando e entendendo</Text>
       <Text style={styles.bigBlue}>just bigBlue</Text>
       <Text style={[styles.bigBlue, styles.red]}>bigBlue, then red</Text>
       <Text style={[styles.red, styles.bigBlue]}>red, then bigBlue</Text>
